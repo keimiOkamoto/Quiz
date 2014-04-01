f8a4c22543dc7a212ab04050e078760ba351649e
keimi <keimiokamoto@gmail.com>
2014-04-01 16:49:02 +0100

    user input test tested with @after, not working

--- setup_client/src/test/java/view/QuizBuilderTest.java
+++ setup_client/src/test/java/view/QuizBuilderTest.java
@@ -1,37 +1,52 @@
 package view;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
 import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.Assert.assertEquals;
-import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.*;
 
 public class QuizBuilderTest {
     private QuizBuilder quizBuilder;
 
     @Rule
-    public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+    public final StandardOutputStreamLog output = new StandardOutputStreamLog();
 
     @Rule
-    public final TextFromStandardInputStream systemInMock = emptyStandardInputStream();
+    public final TextFromStandardInputStream userInput = emptyStandardInputStream();
+    private InputStream stdin;
 
     @Before
     public void buildUp() {
         quizBuilder = new QuizBuilderImpl();
-        quizBuilder.launch();
+
+        stdin = System.in;
+    }
+
+    @After
+    public void tearDown() {
+        System.setIn(stdin);
     }
 
     @Test
     public void shouldDisplayWelcomeMessage() {
         assertEquals("Welcome to the quiz creator. Would you like to create a quiz?\n" +
-                "Please press Y to continue or N to exit.\n", log.getLog());
+                "Please press Y to continue or N to exit.\n", output.getLog());
     }
 
     @Test
     public void shouldGoInToCreateQuizModeIfUserPressesEnter() {
-        systemInMock.provideText("Y");
+        quizBuilder.launch();
+        System.setIn(new ByteArrayInputStream("y\n".getBytes()));
+
+        assertEquals("Welcome to the quiz creator. Would you like to create a quiz?\n" +
+                "Please press Y to continue or N to exit.\nY\n", output.getLog());
     }
 }
