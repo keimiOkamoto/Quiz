17490f72543c679c6ab52bd8ee8ab44e57448f68
keimi <keimiokamoto@gmail.com>
2014-04-04 21:11:57 +0100

    interface for closed quiz container

--- server/src/main/java/items/ClosedQuizContainer.java
+++ server/src/main/java/items/ClosedQuizContainer.java
@@ -1,4 +1,10 @@
 package items;
 
 public interface ClosedQuizContainer {
+    /**
+     * Adds a quiz that has been closed.
+     *
+     * @param closedQuiz A Quiz that has been closed.
+     */
+    void add(Quiz closedQuiz);
 }
