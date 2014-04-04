f797609ea4cfcaf685f36d8bc1b6a175cd569c4f
keimi <keimiokamoto@gmail.com>
2014-04-04 21:11:04 +0100

    the ability to get an answer

--- server/src/main/java/items/Answer.java
+++ server/src/main/java/items/Answer.java
@@ -1,4 +1,11 @@
 package items;
 
 public interface Answer {
+
+    /**
+     * Getter for answer.
+     *
+     * @return A answer String.
+     */
+    String getAnswer();
 }
--- server/src/main/java/items/AnswerImpl.java
+++ server/src/main/java/items/AnswerImpl.java
@@ -1,4 +1,8 @@
 package items;
 
 public class AnswerImpl implements Answer {
+    @Override
+    public String getAnswer() {
+        return null;
+    }
 }
