fda29e5634c7fff0d558036cce70ce19b59490dd
keimi <keimiokamoto@gmail.com>
2014-04-04 21:30:11 +0100

    Items factory test

--- server/src/test/java/items/ItemsFactoryTest.java
+++ server/src/test/java/items/ItemsFactoryTest.java
@@ -1,4 +1,11 @@
 package items;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class ItemsFactoryTest {
+
 }
